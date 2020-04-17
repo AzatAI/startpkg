a3e532c7adeb4d5c3929c22f873a7b40bdfac906
Yaakov Azat <yaakovazat@gmail.com>
2020-04-17 13:29:20 +0600

    updated tge project setup

--- template/setup.py
+++ template/setup.py
@@ -11,29 +11,6 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 # 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist bdist_wheel')
@@ -47,7 +24,7 @@ requires = [
 ]
 
 
-about = {} # create a empty dictionary to store the content of the __version__.py file
+about = {}  # create a empty dictionary to store the content of the __version__.py file
 # execute the __version__.py file and get the content, save to the dictionary about.
 with open(os.path.join(here, 'src', '__version__.py'), 'r', 'utf-8') as f:
     exec(f.read(), about)
@@ -67,7 +44,8 @@ setup(
     author_email=about['__author_email__'],
     url=about['__url__'],
     packages=packages,
-    package_data={'': ['LICENSE'], 'src': ['*.pem'], 'ext': ['*'], 'template': ['*']},
+    package_data={'': ['LICENSE'], 'src': [
+        '*.pem'], 'ext': ['*'], 'template': ['*']},
     package_dir={'startpkg': 'src'},
     include_package_data=True,
     python_requires=">=3.5",
